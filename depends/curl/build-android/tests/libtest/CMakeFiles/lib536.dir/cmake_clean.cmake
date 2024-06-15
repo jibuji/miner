c4bc66de0f2042dccd0cd0a31424d1901607dd45
jibuji <jibuji@proton.me>
2024-06-15 17:51:39 +0800

    add curl depends

---
+++ depends/curl/build-android/tests/libtest/CMakeFiles/lib536.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lib536.dir/first.c.o"
+  "CMakeFiles/lib536.dir/first.c.o.d"
+  "CMakeFiles/lib536.dir/lib536.c.o"
+  "CMakeFiles/lib536.dir/lib536.c.o.d"
+  "lib536"
+  "lib536.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lib536.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ depends/curl/build-android/tests/libtest/CMakeFiles/lib572.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/buji/code/bitbi/miner/depends/curl/tests/libtest/first.c" "tests/libtest/CMakeFiles/lib572.dir/first.c.o" "gcc" "tests/libtest/CMakeFiles/lib572.dir/first.c.o.d"
+  "/home/buji/code/bitbi/miner/depends/curl/tests/libtest/lib572.c" "tests/libtest/CMakeFiles/lib572.dir/lib572.c.o" "gcc" "tests/libtest/CMakeFiles/lib572.dir/lib572.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/buji/code/bitbi/miner/depends/curl/build-android/lib/CMakeFiles/libcurl_static.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
